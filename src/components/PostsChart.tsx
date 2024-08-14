35e974dca288c2801b1c08bd714a890e5873ae3d
Michael Klen <nitrex.code@gmail.com>
2024-08-14 21:20:20 +0200

    populate chart data with users' post counts

--- src/components/PostsChart.tsx
+++ src/components/PostsChart.tsx
@@ -2,12 +2,29 @@ import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { Bar } from 'react-chartjs-2'
 
+import { RootState } from '../redux/store'
+
+interface Post {
+  userId: number
+  id: number
+  title: string
+  body: string
+}
+
+interface User {
+  id: number
+  name: string
+  email: string
+  posts: Post[]
+}
+
 const PostsChart: React.FC = () => {
   const [chartData, setChartData] = useState({})
-  const users = useSelector((state: any) => state.users.users)
+  const users = useSelector((state: RootState) => state.users.users)
+
   useEffect(() => {
-    const userNames = users.map((user: any) => user.name)
-    const postCounts = users.map((user: any) => user.posts.length)
+    const userNames = users.map((user: User) => user.name)
+    const postCounts = users.map((user: User) => user.posts.length)
 
     setChartData({
       labels: userNames,

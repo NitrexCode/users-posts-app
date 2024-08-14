8a867611ce2831f534b7d8a3aa284533dfd1edfa
Michael Klen <nitrex.code@gmail.com>
2024-08-14 21:48:41 +0200

    add components into the app file

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import React from 'react'
 import { Provider } from 'react-redux'
 
 import store from './redux/store'
+import UserList from './components/UserList'
+import PostsChart from './components/PostsChart'
 
 const App: React.FC = () => {
   return (
     <Provider store={store}>
       <div className="App">
         <h1>User List with Posts</h1>
+        <UserList />
+        <PostsChart />
       </div>
     </Provider>
   )

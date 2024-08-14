4d51e024c3e0f505865ab35febc825221ae6d504
Michael Klen <nitrex.code@gmail.com>
2024-08-14 20:56:10 +0200

    add TypeScript types to usersSlice and fetchUsers

--- src/redux/usersSlice.ts
+++ src/redux/usersSlice.ts
@@ -1,22 +1,46 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import axios from 'axios'
 
-export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
-  const response = await axios.get('https://jsonplaceholder.typicode.com/users')
-  const users = await Promise.all(
-    response.data.map(async (user: any) => {
-      const postsResponse = await axios.get(
-        `https://jsonplaceholder.typicode.com/users/${user.id}/posts`,
-      )
-      return { ...user, posts: postsResponse.data }
-    }),
-  )
-  return users
-})
+interface Post {
+  userId: number
+  id: number
+  title: string
+  body: string
+}
+
+interface User {
+  id: number
+  name: string
+  email: string
+  posts: Post[]
+}
+
+export const fetchUsers = createAsyncThunk<User[]>(
+  'users/fetchUsers',
+  async () => {
+    const response = await axios.get(
+      'https://jsonplaceholder.typicode.com/users',
+    )
+    const users = await Promise.all(
+      response.data.map(async (user: User) => {
+        const postsResponse = await axios.get(
+          `https://jsonplaceholder.typicode.com/users/${user.id}/posts`,
+        )
+        return { ...user, posts: postsResponse.data }
+      }),
+    )
+    return users
+  },
+)
+
+const initialState: { users: User[]; status: string } = {
+  users: [],
+  status: 'idle',
+}
 
 const usersSlice = createSlice({
   name: 'users',
-  initialState: { users: [], status: 'idle' },
+  initialState,
   reducers: {},
   extraReducers: (builder) => {
     builder.addCase(fetchUsers.fulfilled, (state, action) => {

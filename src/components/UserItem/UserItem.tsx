cfdfc23dd7542a5a02c9a66602d0c2959a6a32db
Michael Klen <nitrex.code@gmail.com>
2024-08-18 00:41:21 +0200

    added sx for to overwrite the global styles + margin bottom to typography elements

--- src/components/UserItem/UserItem.tsx
+++ src/components/UserItem/UserItem.tsx
@@ -7,26 +7,35 @@ import './UserItem.css'
 
 const UserItem: React.FC<UserItemProps> = ({ user }) => {
   return (
-    <Card className="user-card">
-      <CardContent>
-        <Typography variant="h5" className="user-name">
+    <Card
+      className="user-card border-type-1"
+      sx={{ margin: 'initial', borderRadius: '12px' }}
+    >
+      <CardContent className="user-card-content user-info inner">
+        <Typography mb={1} variant="h5" className="user-name">
           {user.name} ({user.username})
         </Typography>
-        <Typography color="textSecondary">{user.email}</Typography>
-        <Typography color="textSecondary">Phone: {user.phone}</Typography>
-        <Typography color="textSecondary">Website: {user.website}</Typography>
-        <Typography color="textSecondary">
+        <Typography mb={1} color="textSecondary">
+          {user.email}
+        </Typography>
+        <Typography mb={1} color="textSecondary">
+          Phone: {user.phone}
+        </Typography>
+        <Typography mb={1} color="textSecondary">
+          Website: {user.website}
+        </Typography>
+        <Typography mb={1} color="textSecondary">
           Address: {user.address?.street || 'N/A'},{' '}
           {user.address?.suite || 'N/A'}, {user.address?.city || 'N/A'}{' '}
           {user.address?.zipcode || 'N/A'}
         </Typography>
-        <Typography color="textSecondary">
+        <Typography mb={1} color="textSecondary">
           Company: {user.company?.name || 'N/A'}
         </Typography>
-        <Typography color="textSecondary">
+        <Typography mb={1} color="textSecondary">
           Catchphrase: {user.company?.catchPhrase || 'N/A'}
         </Typography>
-        <Typography color="textSecondary">
+        <Typography mb={1} color="textSecondary">
           BS: {user.company?.bs || 'N/A'}
         </Typography>
       </CardContent>

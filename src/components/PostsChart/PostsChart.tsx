be840398689be9a11f06f99e79e02d6dd8ba4671
Michael Klen <nitrex.code@gmail.com>
2024-08-17 15:26:16 +0200

    create averagePostLengths, longestPostLengths statistics and display it in charts

--- src/components/PostsChart/PostsChart.tsx
+++ src/components/PostsChart/PostsChart.tsx
@@ -33,6 +33,32 @@ const PostsChart: React.FC = () => {
     const userNames = users.map((user: User) => user.name)
     const postCounts = users.map((user: User) => user.posts.length)
 
+    const userPostStatistics = users.map((user) => {
+      const postLengths = user.posts.map((post) => post.body.length)
+      const averagePostLength =
+        postLengths.reduce((sum, length) => sum + length, 0) /
+          postLengths.length || 0
+      const longestPost = user.posts.reduce(
+        (longest, post) => {
+          return post.body.length > longest.body.length ? post : longest
+        },
+        user.posts[0] || { body: '', length: 0 },
+      )
+
+      return {
+        userName: user.name,
+        averagePostLength,
+        longestPostLength: longestPost.body.length,
+      }
+    })
+
+    const averagePostLengths = userPostStatistics.map(
+      (stat) => stat.averagePostLength,
+    )
+    const longestPostLengths = userPostStatistics.map(
+      (stat) => stat.longestPostLength,
+    )
+
     setChartData({
       labels: userNames,
       datasets: [
@@ -40,6 +66,16 @@ const PostsChart: React.FC = () => {
           label: 'Number of Posts',
           data: postCounts,
           backgroundColor: 'rgba(75,192,192,1)',
+        },
+        {
+          label: 'Average Post Length',
+          data: averagePostLengths,
+          backgroundColor: 'rgba(153,102,255,1)',
+        },
+        {
+          label: 'Longest Post Length',
+          data: longestPostLengths,
+          backgroundColor: 'rgba(255,159,64,1)',
         },
       ],
     })

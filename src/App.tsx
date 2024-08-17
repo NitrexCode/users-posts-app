142ccd557e6bd33e2496ac1f7522f78ded884e60
Michael Klen <nitrex.code@gmail.com>
2024-08-17 14:58:27 +0200

    change the default structure to the mui components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Provider } from 'react-redux'
+import { Container, Box, Typography } from '@mui/material' // Material-UI components
 
 import store from './redux/store'
 import { UserList } from './components'
@@ -8,11 +9,19 @@ import { PostsChart } from './components'
 const App: React.FC = () => {
   return (
     <Provider store={store}>
-      <div className="App">
-        <h1>User List with Posts</h1>
-        <UserList />
-        <PostsChart />
-      </div>
+      <Container maxWidth="md">
+        <Box sx={{ textAlign: 'center', my: 4 }}>
+          <Typography variant="h4" component="h1" gutterBottom>
+            User List with Posts
+          </Typography>
+        </Box>
+        <Box sx={{ mb: 4 }}>
+          <UserList />
+        </Box>
+        <Box>
+          <PostsChart />
+        </Box>
+      </Container>
     </Provider>
   )
 }
